9499bc49721d4b6a03905ae4f15244671f064988
Alexis <alexisbou16@gmail.com>
2023-04-28 16:12:10 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+function whisper(string) {
+    return string.toLowerCase();
+  }
+
+function logShout(string) {
+    return console.log(string.toUpperCase());
+  }
+
+function logWhisper(string) {
+    return console.log(string.toLowerCase());
+  }
+
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase(string) === string) {
+        return "I can't hear you!"
+    }
+    if (string.toUpperCase(string) === string) {
+        return "YES INDEED!"
+    }
+    if ("Let's have dinner together!" === string) {
+        return "I would love to!"
+    }
+}
